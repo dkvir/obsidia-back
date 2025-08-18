59a9e7fbd522e8b5b4a2c4d86a6e121c88c0be20
dkvir <kvirika137@gmail.com>
2025-08-18 18:19:00 +0400

    👌 IMPROVE: graphql production

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,21 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true, // Enable playground in production
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true, // Enable introspection in production
+        playground: {
+          settings: {
+            "request.credentials": "same-origin",
+          },
+        },
+      },
+    },
+  },
+});
